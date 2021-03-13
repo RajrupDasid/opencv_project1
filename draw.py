9e342bbdc7085e587f06e4ea92e14c38c420eae1
RajrupDasid <rajrupdasid@outlook.com>
2021-03-13 14:47:31 +0530

    Deaw added with text

--- draw.py
+++ draw.py
@@ -10,4 +10,8 @@ cv.imshow('Red',blank)
 #draw a circle
 cv.circle(blank,(blank.shape[1]//2, blank.shape[0]//2),40,(0,0,255),thickness=3)
 cv.imshow('Circle',blank)
+
+# add text on image
+cv.putText(blank,"Hello World",(255,255),cv.FONT_HERSHEY_TRIPLEX,1.0,(0,255,0),2)
+cv.imshow('Text',blank)
 cv.waitKey(0)
