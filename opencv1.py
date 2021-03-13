501de888fed3fbf6d70c8f3e8c705b2b97ff1586
RajrupDasid <rajrupdasid@outlook.com>
2021-03-13 13:00:36 +0530

    Video Resize added

--- opencv1.py
+++ opencv1.py
@@ -1,14 +1,20 @@
 import cv2 as cv
 
-#img = cv.imread('Photos/cat.jpg')
-#cv.imshow('cat', img)
 #reading videos
 capture = cv.VideoCapture('Videos/dog.mp4')
+def rescaleFrame(frame, scale=0.2):
+    width=int(frame.shape[1] * scale)
+    height=int(frame.shape[0] * scale)
+    dimensions = (width,height)
+    return cv.resize(frame,dimensions, interpolation=cv.INTER_AREA)
 while True:
     isTrue, frame= capture.read()
-    cv.imshow('Video',frame)
+    #cv.imshow('Video',frame)
+    frame_resized=rescaleFrame(frame)
+    #cv.imshow('video',frame)
+    cv.imshow('Video Resized',frame_resized)
 
-    if cv.waitKey(20) & 0xFF==ord('d'):
+    if cv.waitKey(20) & 0xFF==ord('e'):
         break
 capture.release()
 cv.destroyAllWindows()
