68904dd9ed6b6fce91aebfccd42f2d1d7d487920
RajrupDasid <rajrupdasid@outlook.com>
2021-03-13 16:47:49 +0530

    Thresholding

---
+++ Threshhold image.py
@@ -0,0 +1,24 @@
+import cv2 as cv
+
+img = cv.imread('Photos/beargrylls.jpg')
+cv.imshow('Manvswild',img)
+#simple thresholding
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray',gray)
+
+#threshholding
+threshold,thresh=cv.threshold(gray,100,255, cv.THRESH_BINARY)
+cv.imshow("Gray Thresholded", thresh)
+
+#thresh binary inverse
+threshold,thresh_inv=cv.threshold(gray,150,255, cv.THRESH_BINARY_INV)
+cv.imshow("Thresholded inverse", thresh_inv)
+
+#Adaptive Thresholding
+
+adaptive_thresh = cv.adaptiveThreshold(gray,255,cv.ADAPTIVE_THRESH_MEAN_C,cv.THRESH_BINARY,11,3)
+cv.imshow('Adaptive Thresholdting', adaptive_thresh)
+
+
+
+cv.waitKey(0)
