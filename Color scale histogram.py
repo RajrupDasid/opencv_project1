cb4d8ce5f096909494ffd10502693cc25ae407a6
RajrupDasid <rajrupdasid@outlook.com>
2021-03-13 16:25:09 +0530

    Color Scale Histogram using masked

--- Color scale histogram.py
+++ Color scale histogram.py
@@ -4,10 +4,9 @@ import matplotlib.pyplot as plt
 img = cv.imread('Photos/Bear.jpeg')
 cv.imshow('Bear', img)
 blank = np.zeros(img.shape[:2], dtype='uint8')
-circle= cv.circle(blank,(img.shape[1]//2 +30 ,img.shape[0]//2 -110 ), 190, 255, -1)
-mask= cv.bitwise_and( img, img, mask=circle)
-cv.imshow('Mask',mask)
-
+mask= cv.circle(blank,(img.shape[1]//2 +30 ,img.shape[0]//2 -110 ), 190, 255, -1)
+masked= cv.bitwise_and( img, img, mask=mask)
+cv.imshow('Mask',masked)
 #color histogram
 plt.figure()
 plt.title('Color scale histogram')
@@ -15,7 +14,7 @@ plt.xlabel('Bins')
 plt.ylabel('# of pixels')
 colors = ('b','g','r')
 for i,col in enumerate(colors):
-    hist=cv.calcHist([img],[i],None,[256],[0,256])
+    hist=cv.calcHist([img],[i], mask,[256],[0,256])
     plt.plot(hist,color=col)
     plt.xlim([0,256])
 
